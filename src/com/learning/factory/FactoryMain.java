266a69989e1565112053bf995f79d6162c196cfd
Raghu <raghugre@gmail.com>
2020-05-11 11:21:40 +0530

    Initial Changes

---
+++ src/com/learning/factory/FactoryMain.java
@@ -0,0 +1,17 @@
+package com.learning.factory;
+
+import java.io.File;
+
+public class FactoryMain {
+
+	public static void main(String[] args) {
+
+		File file = new File("D:\\Personal\\Monthly.csv");
+		FileProcessor fileProcessor = FileProcessorFactory.getFileProcessor(file);
+		if(fileProcessor.validateFile(file)) {
+			fileProcessor.fileProcess(file);
+		}
+		
+	}
+
+}
