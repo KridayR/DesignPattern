c9bcda48104905402f6bd06557e49a70c701385b
Raghu <raghugre@gmail.com>
2020-05-23 22:53:47 +0530

    Added missed files

---
+++ src/com/learning/prototype/ShapeCache.java
@@ -0,0 +1,34 @@
+package com.learning.prototype;
+
+import java.util.Hashtable;
+
+public class ShapeCache {
+
+	private static Hashtable<String, Shape> shapeMap = new Hashtable<String, Shape>();
+
+	public static void loadCache() {
+
+		Circle circle = new Circle();
+		circle.setId("1");
+		shapeMap.put(circle.getId(), circle);
+
+		Square square = new Square();
+		square.setId("2");
+		shapeMap.put(square.getId(), square);
+
+		Rectangle rectangle = new Rectangle();
+		rectangle.setId("3");
+		shapeMap.put(rectangle.getId(), rectangle);
+		
+		Hexagon hexagon = new Hexagon();
+		hexagon.setId("4");
+		shapeMap.put(hexagon.getId(), hexagon);
+
+	}
+
+	public static Shape getShape(String shapeId) {
+		Shape cachedShape = shapeMap.get(shapeId);
+		return (Shape) cachedShape.clone();
+	}
+
+}
